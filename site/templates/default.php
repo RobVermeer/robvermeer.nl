91e089044ea888aa6c61a7e9afc26ea8dec49f75
Rob Vermeer <rob@vermeertech.nl>
2014-04-18 22:28:35 +0200

    Add default tamplate

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,3 +1,12 @@
 <?php snippet('header') ?>
+	<section class="content">
+		<nav>
+			<a href="<?php echo c::get('url'); ?>">Back to home</a>
+		</nav>
+		<article>
+			<h1><?php echo html($page->title()) ?></h1>
+			<?php echo kirbytext($page->text()) ?>
+		</article>
+	</section>
 
 <?php snippet('footer') ?>
