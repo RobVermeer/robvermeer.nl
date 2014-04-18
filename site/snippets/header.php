7969fc75e326a217418262742436a6afd1a43462
Rob Vermeer <rob@vermeertech.nl>
2014-04-18 22:05:27 +0200

    Add repsonsive heading

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,6 +7,7 @@
 	<meta name="description" content="<?php echo html($site->description()) ?>" />
 	<meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
 	<meta name="robots" content="index, follow" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<?php echo css('//fonts.googleapis.com/css?family=Ubuntu+Mono:400,700'); ?>
 	<?php echo css('assets/css/style.css'); ?>
