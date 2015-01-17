082a82e0a8aefe330402c6c4afe1d5e93e92575c
Rob Vermeer <rob@vermeertech.nl>
2015-01-17 22:18:22 +0100

    Add see more link

--- site/snippets/leader.php
+++ site/snippets/leader.php
@@ -3,8 +3,8 @@
 			<h1><a href="#article" class="jump"><?php echo html($site->title()) ?></a></h1>
 			<h3><?php echo html($site->tagline()) ?></h3>
 			<?php echo str::email($site->email(), "Contact", "Contact me", "btn") ?>
-			
 		</nav>
+		<a href="#article" class="jump see-more"><i class="fa fa-arrow-circle-down"></i></a>
 	</header>
 	<div class="why-dont-you-hire-me"></div>
 	
